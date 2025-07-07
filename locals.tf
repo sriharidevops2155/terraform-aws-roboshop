ae4867b852ed82c06f04bb7bd0e2cf1c2ef6feb9
sriharibandi <shreharii@gmail.com>
2025-07-08 00:24:10 +0530

    terraform-aws-roboshop

--- locals.tf
+++ locals.tf
@@ -4,16 +4,16 @@ locals {
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   
-  backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listner_arn
-  frontend_alb_listner_arn = data.aws_ssm_parameter.frontend_alb_listner_arn
+  backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listner_arn.value
+  frontend_alb_listner_arn = data.aws_ssm_parameter.frontend_alb_listner_arn.value
   
   ami_id = data.aws_ami.rhel.id
   sg_id = data.aws_ssm_parameter.sg_id.value
 
-  alb_listner_arn = "${var.component}" == "frontend" ?  local.frontend_alb_listner_arn.value : local.backend_alb_listner_arn.value
+  alb_listner_arn = "${var.component}" == "frontend" ?  local.frontend_alb_listner_arn : local.backend_alb_listner_arn
   tg_port = "${var.component}" == "frontend" ? 80 : 8080
   health_check = "${var.component}" == "frontend" ? "/" : "/health"
-  rule_header_url = "${var.component}" == "frontend" ? "${var.environment}.${var.zone_name}" : "${var.component}.backend-${var.environment}-${var.zone_id}"
+  rule_header_url = "${var.component}" == "frontend" ? "${var.environment}.${var.zone_name}" : "${var.component}.backend-${var.environment}.${var.zone_id}"
 
   common_tags = {
     Project = var.project
